6d7df878a415f2f4ca9f6107eab57fc441e91a57
MatheusGualtieri <50122650+MatheusGualtieri@users.noreply.github.com>
2024-03-20 21:56:04 -0300

    feat: add dark/ligth mode to header

--- front/src/components/Header/index.tsx
+++ front/src/components/Header/index.tsx
@@ -3,14 +3,14 @@ import { ButtonSwitch } from "../ButtonSwitch";
 
 export const Header = () => {
   return (
-    <div className="w-full h-12 flex bg-stone-900 justify-items-center justify-center border-b-2 border-rose-400">
+    <div className="sticky top-0 w-full h-12 flex dark:bg-stone-900 justify-items-center justify-center border-b-2 border-rose-400 transition-all duration-300">
       <div className="px-8 w-full flex justify-between items-center h-full md:px-0 md:max-w-[700px] xl:max-w-[1000px]">
         <div className="">
-          <h1 className="text-stone-50">
+          <h1 className="dark:text-stone-50">
             My<span className="text-rose-400">Portfolio</span>
           </h1>
         </div>
-        <nav className="hidden z-1 text-stone-50 gap-11 md:flex md:z-0">
+        <nav className="hidden z-1 dark:text-stone-50 gap-11 md:flex md:z-0">
           <ButtonA buttonText="Sobre Mim" />
           <ButtonA buttonText="Projetos" />
           <ButtonA buttonText="Contatos" />
