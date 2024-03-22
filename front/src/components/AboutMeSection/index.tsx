4bcbeace3458c3bca4f30f469dd5b332db3454ca
MatheusGualtieri <50122650+MatheusGualtieri@users.noreply.github.com>
2024-03-21 21:39:19 -0300

    feat: add dark mode to about me section

--- front/src/components/AboutMeSection/index.tsx
+++ front/src/components/AboutMeSection/index.tsx
@@ -4,14 +4,18 @@ export const AboutMeSection = () => {
   return (
     <div className="flex justify-center w-full">
       <div className="my-8 box-border px-8 w-full max-w-[1200px] h-auto flex flex-col gap-6 md:">
-        <h1 className="text-center text-3xl">Sobre mim</h1>
-        <p>
+        <h1 className="text-stone-900 dark:text-stone-50 text-center text-3xl">
+          Sobre mim
+        </h1>
+        <p className="text-stone-600 dark:text-stone-300">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat
           eveniet eum facere veritatis labore consequatur. Cupiditate quia magni
           perspiciatis iste, soluta quas quaerat dolorem obcaecati dolorum, iure
           doloremque atque! Ex?
         </p>
-        <h2 className="text-center text-2xl">Habilidades</h2>
+        <h2 className="text-stone-900 dark:text-stone-50 text-center text-2xl">
+          Habilidades
+        </h2>
         <TechList />
       </div>
     </div>
--- front/src/components/TechCard/index.tsx
+++ front/src/components/TechCard/index.tsx
@@ -6,7 +6,7 @@ export const TechCard = (item: ITechs) => {
       key={item.name}
       className="flex flex-col gap-1 justify-center items-center text-center"
     >
-      <h3>{item.name}</h3>
+      <h3 className="text-stone-600 dark:text-stone-300">{item.name}</h3>
       <item.logo className="text-rose-400 border-b-2 border-rose-400 w-14 h-14"></item.logo>
     </li>
   );
